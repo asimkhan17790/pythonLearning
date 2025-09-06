f9cc004f941e951ab0858a4c1c0d878e049d019b
Asim Khan <asim.khan17790@gmail.com>
2025-09-06 02:25:44 -0400

    drink water reminder app

---
+++ my_modules/python_projects/drink-water-reminder/main.py
@@ -0,0 +1,33 @@
+import time
+import asyncio
+import signal
+# from plyer import notification
+# plyer does not work on MacOS
+import pync
+from desktop_notifier import DesktopNotifier, Urgency, Button, ReplyField, DEFAULT_SOUND
+
+
+async def start_notification_app():
+
+    while True:
+
+        # notification.notify(title="Please drink some water",
+        #                     message="You need to drink some water.... Its been an hour")
+
+        # pync.notify("Please Drink Water",
+        #            title="!!!REMINDER!!!", sound="Horn")
+
+        notifier = DesktopNotifier(app_name="Reminder App")
+        await notifier.send(
+            title="Please Drink Water",
+            message="Hey Asim, Dont forget to drink some water",
+            urgency=Urgency.Critical,
+            on_dispatched=lambda: print("Notification showing"),
+            sound=DEFAULT_SOUND,
+        )
+        time.sleep(10)
+
+
+if __name__ == "__main__":
+    asyncio.run(start_notification_app())
+   # start_notification_app()
