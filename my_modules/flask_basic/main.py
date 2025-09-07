569342b921bb5681d142e7423c3f06042f7bd027
Asim Khan <asim.khan17790@gmail.com>
2025-09-07 14:41:05 -0400

    flass app example

--- my_modules/flask_basic/main.py
+++ my_modules/flask_basic/main.py
@@ -1,3 +1,5 @@
+# Copilot recommendation manual --> OPTION + \
+
 # If choosing separate environment for your app:
 # To set environment:
 #    source .venv/bin/activate
@@ -8,19 +10,55 @@
 
 
 # You can also run the code like below:
-# To run from terminal using the flask command
-#   flask --app main run
+# To run from terminal using the flask command:
+#   flask --app main run. - bring server up on port 5000 by default
+#   flask --app main run --host=localhost --port=9000. -- can provide custom port
 
 
-from flask import Flask
+from flask import Flask, render_template, send_from_directory, request
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello Asim... Lets do this!!</p>"
+@app.route('/download/<filename>')
+def download_image(filename):
+    # The directory where the files are located (your static folder)
+    directory = 'static'
+    filename = "".join((filename, ".jpeg"))
+    # Use send_from_directory to serve the file and prompt download
+    return send_from_directory(directory, filename, as_attachment=True)
+
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    print(request.method)
+
+    if (request.method == "POST"):
+        print("POST METHOD CALLED")
+        print(request.form)
+        print(f"{request.form['fullName']} {request.form['email']}")
+        # with open("file.txt", "w") as f:
+        #    f.write(f"{request.form['fullName']} {request.form['email']}")
+
+    return render_template("home.html")
+
+
+@app.route("/services")
+def services():
+    return render_template("services.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # to run the code directly from python
+    print("RUNNING FROM MAIN FUNCTION")
+    app.run(host='localhost', port=9000, debug=True)
